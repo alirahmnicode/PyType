cfbd7977f2b20f481e2c255aba146f75f94052ab
alirahmanicode <alirahmanicode@gmail.com>
2022-01-28 18:31:14 +0330

    get time and correction text

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-from package import text
+from package import time, text, score
 
 
 class Main:
     def text_p(self):
         t = text.Text()
+
         # give text to user
         t.give_the_text()
+
+        # get start time
+        get_time = time.Time()
+        get_time.get_start_time()
+
         # get user text
         user_text = t.get_the_user_text()
+
+        # get end time
+        get_time.get_end_time()
+
         # Correction user text
-        t.correction_text()
+        correction = t.correction_text()
+
+        # delay time
+        secounds = get_time.get_delay_time()
+        print(secounds)
+
+        # calculate score
+        get_score = score.Score(
+            correction['less_word_count'], correction['wrong_count'], secounds)
+        
+        calculate = get_score.p()
 
 
 if __name__ == '__main__':
     start = Main()
     start.text_p()
-
-
